2ef00eef7f8d7c1ca0053600386216e4011e6101
shooter <shooter.hero@gmail.com>
2019-08-10 11:00:00 +0800

    008

---
+++ src/TodoList.js
@@ -0,0 +1,91 @@
+import React from "react"
+
+class TodoList extends React.Component {
+  state = {
+    todos: [],
+    newTodo: "",
+  }
+
+  handleCheckboxClick(id) {
+    let newTodos = [...this.state.todos]
+    newTodos[newTodos.findIndex(todo => todo.id === id)].done = true
+    this.setState({
+      todos: newTodos,
+    })
+  }
+
+  handleAddTodoClick = e => {
+    e.preventDefault()
+    const newTodo = {
+      id: this.state.todos.length + 1,
+      title: this.state.newTodo,
+      done: false,
+    }
+    const todos = [...this.state.todos]
+    todos.push(newTodo)
+    this.setState({
+      todos: todos,
+      newTodo: "",
+    })
+  }
+
+  hanldeInputChange = e => {
+    this.setState({
+      newTodo: e.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div
+        style={{ padding: "30px 0" }}
+        className="ui text container center aligned"
+      >
+        <h2>My Todos</h2>
+        <div className="ui grid">
+          <div className="row centered">
+            <div className="column twelve wide">
+              <form className="ui form" onSubmit={this.handleAddTodoClick}>
+                <div className="inline fields">
+                  <div className="twelve wide field">
+                    <input
+                      type="text"
+                      value={this.state.newTodo}
+                      onChange={this.hanldeInputChange}
+                    />
+                  </div>
+                  <button className="ui button primary" type="submit">
+                    Add todo
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="row centered">
+            <div className="column twelve wide">
+              <div className="grouped fields">
+                {this.state.todos
+                  .filter(todo => !todo.done)
+                  .map(todo => (
+                    <div key={todo.id} className="field">
+                      <div className="ui checkbox">
+                        <input
+                          type="checkbox"
+                          onClick={() => {
+                            this.handleCheckboxClick(todo.id)
+                          }}
+                        />
+                        <label>{todo.title}</label>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TodoList
