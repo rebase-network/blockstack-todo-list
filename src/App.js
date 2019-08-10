f8902640fd5fccd5f2028946237e0b05ea4f9b92
shooter <shooter.hero@gmail.com>
2019-08-10 09:41:30 +0800

    002

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from "react"
 
 class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Wash the dishes",
+        done: false,
+      },
+      {
+        id: 2,
+        title: "Clean my room",
+        done: false,
+      },
+    ],
+  }
+
   render() {
-    return <div>Nice Meme</div>
+    return (
+      <div style={{ padding: "30px 0" }}
+          className="ui text container center aligned">
+          <h2>My Todos</h2>
+          <div className="ui grid">
+            <div className="row centered">
+              <div className="column twelve wide">
+                <div className="grouped fields">
+                  {this.state.todos
+                    .filter(todo => !todo.done)
+                    .map(todo => (
+                      <div key={todo.id} className="field">
+                        <div className="ui checkbox">
+                          <input type="checkbox" />
+                          <label>{todo.title}</label>
+                        </div>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
   }
 }
 
-export default App
+export default App;
